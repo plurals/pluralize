3cf8c4ce3098a97b5ead24a0bbbdc785e060a953
Peter Keller <petekeller2@gmail.com>
2017-04-30 13:29:48 -0400

    IS_PLURAL_TESTS and IS_SINGULAR_TESTS arrays removed

--- test.js
+++ test.js
@@ -618,32 +618,6 @@ var PLURAL_TESTS = [
 ];
 
 /**
- * Is plural tests.
- *
- * @type {Array}
- */
-var IS_PLURAL_TESTS = [
-  ['whisky', false],
-  ['echos', true],
-  ['bluffs', true],
-  ['incentive', false],
-  ['sleeve', false]
-];
-
-/**
- * Is singular tests.
- *
- * @type {Array}
- */
-var IS_SINGULAR_TESTS = [
-  ['whisky', true],
-  ['echos', false],
-  ['bluffs', false],
-  ['incentive', true],
-  ['sleeve', true]
-];
-
-/**
  * Test suite.
  */
 describe('pluralize', function () {
@@ -665,17 +639,17 @@ describe('pluralize', function () {
     });
 
     describe('isPlural', function () {
-      IS_PLURAL_TESTS.forEach(function (test) {
-        it(test[0] + ' is plural: ' + test[1], function () {
-          expect(pluralize.isPlural(test[0])).to.equal(test[1]);
+      BASIC_TESTS.concat(PLURAL_TESTS).forEach(function (test) {
+        it(test[1] + ' is plural', function () {
+          expect(pluralize.isPlural(test[1])).to.equal(true);
         });
       });
     });
 
     describe('isSingular', function () {
-      IS_SINGULAR_TESTS.forEach(function (test) {
-        it(test[0] + ' is singular: ' + test[1], function () {
-          expect(pluralize.isSingular(test[0])).to.equal(test[1]);
+      BASIC_TESTS.concat(SINGULAR_TESTS).forEach(function (test) {
+        it(test[0] + ' is singular', function () {
+          expect(pluralize.isSingular(test[0])).to.equal(true);
         });
       });
     });
